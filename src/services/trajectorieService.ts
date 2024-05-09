d4a67716c779d9a1218be93004f8aa76261228ad
Julia Cruz <131014598+jjuliacp@users.noreply.github.com>
2024-05-09 12:26:19 -0500

    agregar funcionalidad para consultar ubicaciones por id y date

--- src/services/trajectorieService.ts
+++ src/services/trajectorieService.ts
@@ -2,25 +2,28 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 //función que maneje las ubicaciones de los taxis segun id y date
-export const getTaxiLocations = async (taxiId: string, searchDate: Date) {
+export const getTaxiLocations = async (taxiId: string, searchDate: Date, startIndex: number, limit: number) => {
 
     try {
         const taxiLocations = await prisma.trajectories.findMany({
-            // where: {
-            //   taxi_id: parseInt(taxiId),
-            //   timestamp: {
-            //     gte: searchDate, // Fecha mayor o igual a la fecha especificada
-            //     lt: new Date(searchDate.getTime() + 24 * 60 * 60 * 1000) // Fecha menor a 24 horas después de la fecha especificada
-            //   }
-            // },
-            // orderBy: {
-            //   timestamp: 'asc' // Ordenar por fecha ascendente
-            // }
+            skip: startIndex,
+            take: limit,
+            where: {
+              taxi_id: parseInt(taxiId),
+              date: {
+                gte: searchDate, // Fecha mayor o igual a la fecha especificada 
+                lt: new Date(searchDate.getTime() + 24 * 60 * 60 * 1000) // Fecha menor a 24 horas después de la fecha especificada
+              }
+            },
+            orderBy: {
+              date: 'asc' // Ordenar por fecha ascendente
+            }
         });
 
         return taxiLocations
     }
     catch (error) {
         console.error('Error al obtener las ubicaciones del taxi', error);
+        return
     }
 }
