8f32ed7e1828ace7a223afcaa8a322fe39682645
Julia Cruz <131014598+jjuliacp@users.noreply.github.com>
2024-05-13 12:53:07 -0500

    añadir test para el endpoint de historial de ubicaciones

--- src/controller/trajectories.ts
+++ src/controller/trajectories.ts
@@ -7,7 +7,7 @@ export const locationLog: Handler = async (req, res) => {
   const { id } = req.params
   const { date } = req.query
   if (!id || !date) {
-    res.status(400).json({ message: 'los parametros taxiId y date son obligatorios en la consulta' })
+    return res.status(400).json({ message: 'los parametros taxiId y date son obligatorios en la consulta' })
   }
   const page = parseInt(req.query.page as string) || 1; // Página por defecto 1
   const limit = parseInt(req.query.limit as string) || 10; // Límite por defecto 10
@@ -22,11 +22,11 @@ export const locationLog: Handler = async (req, res) => {
     // Esto depende de cuántas ubicaciones de taxi se esperen para una fecha determinada
 
     // Enviar respuesta con las ubicaciones del taxi
-    res.status(200).json(taxiLocation);
+    return res.status(200).json(taxiLocation);
 
   } catch (error) {
-    console.error('Error al obtener las ubicaciones del taxi', error);
-    res.status(500).json({ message: 'Error interno del servidor' });
+   // console.error('Error al obtener las ubicaciones del taxi', error);
+    return res.status(500).json({ message: 'Error interno del servidor' });
   }
 }
 
--- src/services/trajectorieService.ts
+++ src/services/trajectorieService.ts
@@ -2,16 +2,16 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 //función que maneje las ubicaciones de los taxis segun id y date
-export const getTaxiLocations = async (taxiId: string, searchDate: Date, startIndex: number, limit: number) => {
+export const getTaxiLocations = async (id: string, searchDate: Date, startIndex: number, limit: number) => {
 
     try {
-        const taxiLocations = await prisma.trajectories.findMany({
+      return  await prisma.trajectories.findMany({
             skip: startIndex,
             take: limit,
             where: {
-              taxi_id: parseInt(taxiId),
+              taxi_id: parseInt(id),
               date: {
-                gte: searchDate, // Fecha mayor o igual a la fecha especificada 
+                gte: searchDate, // Fecha mayor o igual a la fecha especificada // todas las ubis que sean >= a la fecha
                 lt: new Date(searchDate.getTime() + 24 * 60 * 60 * 1000) // Fecha menor a 24 horas después de la fecha especificada
               }
             },
@@ -20,10 +20,9 @@ export const getTaxiLocations = async (taxiId: string, searchDate: Date, startIn
             }
         });
 
-        return taxiLocations
     }
     catch (error) {
-        console.error('Error al obtener las ubicaciones del taxi', error);
-        return
+        //console.error('Error al obtener las ubicaciones del taxi', error);
+        return error
     }
 }
